478a3f5c3d6b0397e8d071930d460be1eb9ebf9c
Kumar shanu <funnyclups413@gmail.com>
2024-06-27 10:43:04 +0530

    added search

--- src/pages/Homepage.tsx
+++ src/pages/Homepage.tsx
@@ -1,10 +1,17 @@
 import landingImage from "../assets/landing.png";
 import appDownloadImage from "../assets/appDownload.png";
+import SearchBar, { SearchForm } from "@/components/Searchbar";
+import { useNavigate } from "react-router-dom";
 
 
 
 const HomePage = () => {
-  
+  const navigate = useNavigate();
+  const handleSearchSubmit = (searchFormValues: SearchForm) => {
+    navigate({
+      pathname: `/search/${searchFormValues.searchQuery}`,
+    });
+  };
 
   return (
     <div className="flex flex-col gap-12">
@@ -12,6 +19,11 @@ const HomePage = () => {
         <h1 className="text-5xl font-bold tracking-tight text-orange-600">
           Tuck into a takeway today
         </h1>
+        <span className="text-xl">Food is just a click away!</span>
+        <SearchBar
+          placeHolder="Search by City or Town"
+          onSubmit={handleSearchSubmit}
+        />
        
       </div>
       <div className="grid md:grid-cols-2 gap-5">
